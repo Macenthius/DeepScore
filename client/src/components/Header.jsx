15bca327b15607b6b612379afe81344a752028fa
Macenthius <divijkulk.ace7@gmail.com>
2024-03-20 21:33:26 +0530

    Its done. All done. [master 768025d

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 
-const Header = ({ heading }) => {
-    return (
+const Header = ({heading}) => {
+    return(
         <div className='bg-gray-800 flex justify-center items-center h-16 w-full fixed top-0'>
             <h1 className='text-3xl text-white font-bold mb-4 pl-16 text-center rounded-lg'>
                 {heading}
             </h1>
+
         </div>
-    );
+    )
 };
 
 export default Header;
